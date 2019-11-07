44da6f830a876e3c05370cc22c65172896fab71e
hujishima takumi <hujishima.t425@gmail.com>
2019-11-07 14:52:56 +0900

    crate testScript

---
+++ game-VGA-tarm4/Assets/Script/testScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    int test = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(test);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
