d265c2c26979bd9feab7b94b479a7bfe9532e379
hujishima takumi <hujishima.t425@gmail.com>
2019-11-07 14:57:48 +0900

    change testScript

--- game-VGA-tarm4/Assets/Script/testScript.cs
+++ game-VGA-tarm4/Assets/Script/testScript.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class testScript : MonoBehaviour
 {
     int test = 100;
+    int a = 200;
+    int b = 300;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(test);
+        Debug.Log(a + b);
     }
 
     // Update is called once per frame
